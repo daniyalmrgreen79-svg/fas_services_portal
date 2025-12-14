278a8465dad5b2fc2f702c2eda2d3fb127f425e7
daniyalmrgreen79-svg <daniyalmr.green79@gmail.com>
2025-12-14 22:14:59 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,3 +1,10 @@
+import 'package:flutter/material.dart';
+
 void main() {
-  print('FAS Services Portal');
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text("FAS Services Portal")),
+      body: Center(child: Text("Welcome to FAS Services Portal!")),
+    ),
+  ));
 }
